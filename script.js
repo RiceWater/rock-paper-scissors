1eb4b152c67816f87ac0ffd1c0bb8965b2b1563b
Will <willianchua.wc@gmail.com>
2024-01-12 11:32:44 +0800

    Add html elements and logic
    RPS can now be played on the browser. The score resets once it reaches 5 and announces the victor of the game. Functions were added and simplified to follow function principle of one task only.
    
    I plan to make a button that would restart the entire game once the victor is announced. Also, it lets the player see who scored 5 aside from the victor announcement.

--- script.js
+++ script.js
@@ -15,8 +15,7 @@ function getComputerChoice(){
 }
 
 function playGame(playerChoice, computerChoice){
-    playerChoice = playerChoice.toLowerCase();
-
+    playerChoice = playerChoice.toLowerCase();  
     if (playerChoice == computerChoice){
         return "Draw";
     }
@@ -30,8 +29,73 @@ function playGame(playerChoice, computerChoice){
     }
 }
 
-for(let i = 0; i < 5; i++){
-    let choice = prompt("Rock, paper, or scissors?")
-    compChoice = getComputerChoice();
-    console.log(playGame(choice, compChoice));
+function updateScores(winner){
+    const playerScore = document.querySelector("#playerScore");
+    const compScore = document.querySelector("#compScore");
+    let pScoreInt = parseInt(playerScore.textContent);
+    let cScoreInt = parseInt(compScore.textContent);
+    
+    switch (winner){
+        case "player":
+            playerScore.textContent = ++pScoreInt;
+            break;
+        case "comp":
+            compScore.textContent = ++cScoreInt;
+            break;
+        default:
+            break;
+    }
+
+    if (pScoreInt >= 5){
+        playerScore.textContent = 0;
+        compScore.textContent = 0;
+        return announceVictor("player");
+    } 
+    if (cScoreInt >= 5){
+        playerScore.textContent = 0;
+        compScore.textContent = 0;
+        return announceVictor("comp");
+    }
+    return "";
 }
+
+function announceVictor(victor){
+    switch (victor){
+        case "player":
+            return "YOU ARE THE VICTOR";
+        case "comp":
+            return "COMPUTER IS THE VICTOR"
+        default:
+            return "";
+    }
+}
+const menu = document.querySelector("#choices");
+menu.addEventListener('click', (event) => {
+    const tar = event.target.id;
+    switch (tar){
+        case ROCK:
+            playRound(ROCK);
+            break;
+        case SCISSORS:
+            playRound(SCISSORS)
+            break;
+        default:
+            playRound(PAPER);
+            break;        
+    }
+})
+
+function playRound(playerChoice){
+    const compChoice = getComputerChoice();
+    const resultDiv = document.querySelector("#result");
+    const res = playGame(playerChoice, compChoice);
+
+    const victor = (res === "Player Wins!") ? updateScores("player") : updateScores("comp");
+    resultDiv.textContent = victor;
+
+}
+// for(let i = 0; i < 5; i++){
+//     let choice = prompt("Rock, paper, or scissors?")
+//     compChoice = getComputerChoice();
+//     console.log(playGame(choice, compChoice));
+// }
